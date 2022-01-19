6826fca58bea29e3489e5e9e09fa7f57bf166cb9
mnjrupp <mnjrupp@hotmail.com>
2022-01-19 15:56:53 -0600

    2 files modified

--- src/app/loanofficer/loanofficer.component.ts
+++ src/app/loanofficer/loanofficer.component.ts
@@ -12,7 +12,7 @@ import  * as dbOfficers from '../../assets/Officers.json';
   styleUrls: ['./loanofficer.component.css']
 })
 export class LoanOfficerComponent implements OnInit {
-  //officers:LoanOfficer[] = dbOfficers;
+  officers = dbOfficers;
   formValue : FormGroup;
   costoffundsreqObj: CostOfFundsRequest;
 
