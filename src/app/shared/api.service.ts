fddcf984e828414a610ba671b7e7bc5233dcead2
mnjrupp <mnjrupp@hotmail.com>
2022-03-31 14:33:51 -0500

    1 file modified

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   /*This is the FCBTCostOfFunds API */
-  postCostofFunds(data: any) {
+  postCostofFunds(data: any): Observable<any> {
     return this.http.post<any>("http://lstsqldev01.nterprise.net:8082/api/FCBTCostOfFunds", data)
       .pipe(map((res: any) => {
         return res;
